93d100ccfeb69fec7913cb969812b4a5258f588f
Choonster TheMage <Choonster.2010@gmail.com>
2017-06-07 15:11:30 +1000

    Add textures from ContinuousQ of Curse

--- config.lua
+++ config.lua
@@ -19,7 +19,7 @@ ns.CONFIG = CONFIG
 -------
 
 -- The path of the texture file you want to use relative to the main WoW directory (without the texture's file extension).
--- The AddOn includes twenty one textures:
+-- The AddOn includes thirty textures:
 --	Reticule				-- Red targeting reticle (contributed by Dridzt of WoWI)
 --	RedArrow				-- Red arrow pointing downwards (contributed by DohNotAgain of WoWI)
 --	NeonReticule			-- Neon version of the reticule (contributed by mezmorizedck of Curse)
@@ -41,6 +41,15 @@ ns.CONFIG = CONFIG
 --	PurpleArrow				-- Abstract style purple arrow pointing downwards, same style as bluearrow1 (contributed by Imithat of WoWI)
 --	Shield					-- Kite shield with sword and crossed spears/polearms (contributed by Imithat of WoWI)
 --	NeonGreenArrow			-- Green version of the neon red arrow (contributed by Nokiya420 of Curse)
+--	Q_FelFlamingSkull		-- Fel green flaming skull (contributed by ContinuousQ of Curse)
+--	Q_RedFlamingSkull		-- Red flaming skull (contributed by ContinuousQ of Curse)
+--	Q_ShadowFlamingSkull	-- Shadow purple flaming skull (contributed by ContinuousQ of Curse)
+--	Q_GreenGPS				-- Green map pin/GPS symbol (contributed by ContinuousQ of Curse)
+--	Q_RedGPS				-- Red map pin/GPS symbol (contributed by ContinuousQ of Curse)
+--	Q_WhiteGPS				-- White map pin/GPS symbol (contributed by ContinuousQ of Curse)
+--	Q_GreenTarget			-- Green target arrow (contributed by ContinuousQ of Curse)
+--	Q_RedTarget				-- Red target arrow (contributed by ContinuousQ of Curse)
+--	Q_WhiteTarget			-- White target arrow (contributed by ContinuousQ of Curse)
 
 -- All of the textures listed above need to be prefixed with "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\" like the default value below.
 CONFIG.TEXTURE_PATH = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\Reticule"
