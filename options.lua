a06ffbead03a3884d06f00d0c6b7d164ec8f58b1
Choonster TheMage <Choonster.2010@gmail.com>
2018-08-19 00:03:39 +1000

    Add localisations

--- options.lua
+++ options.lua
@@ -7,63 +7,72 @@ local ACD = LibStub("AceConfigDialog-3.0")
 local ACC = LibStub("AceConfigCmd-3.0")
 local frameref
 
+local L = LibStub("AceLocale-3.0"):GetLocale(addon)
+
 -- The indicator textures provided by the AddOn. Used for the texture dropdowns.
-local TEXTURES = {
-	["Reticule"]             = "Reticule - Red targeting reticule",
-	["RedArrow"]             = "RedArrow - Red arrow pointing downwards",
-	["NeonReticule"]         = "NeonReticule - Neon version of the reticule",
-	["NeonRedArrow"]         = "NeonRedArrow - Neon version of the red arrow",
-	["RedChevronArrow"]      = "RedChevronArrow - Red inverted triple chevron",
-	["PaleRedChevronArrow"]  = "PaleRedChevronArrow - Pale red version of the chevron",
-	["arrow_tip_green"]      = "arrow_tip_green - Green 3D arrow",
-	["arrow_tip_red"]        = "arrow_tip_red - Red 3D arrow",
-	["skull"]                = "skull - Skull and crossbones",
-	["circles_target"]       = "circles_target - Red concentric circles in the style of a target",
-	["red_star"]             = "red_star - Red star with gold outline",
-	["greenarrowtarget"]     = "greenarrowtarget - Neon green arrow with a red target",
-	["BlueArrow"]            = "BlueArrow - Blue arrow pointing downwards",
-	["bluearrow1"]           = "bluearrow1 - Abstract style blue arrow pointing downwards",
-	["gearsofwar"]           = "gearsofwar - Gears of War logo",
-	["malthael"]             = "malthael - Malthael (Diablo) logo",
-	["NewRedArrow"]          = "NewRedArrow - Red arrow pointing downwards",
-	["NewSkull"]             = "NewSkull - Skull with gas mask",
-	["PurpleArrow"]          = "PurpleArrow - Abstract style purple arrow pointing downwards",
-	["Shield"]               = "Shield - Kite shield with sword and crossed spears/polearms",
-	["NeonGreenArrow"]       = "NeonGreenArrow - Green version of the neon red arrow",
-	["Q_FelFlamingSkull"]    = "Q_FelFlamingSkull - Fel green flaming skull",
-	["Q_RedFlamingSkull"]    = "Q_RedFlamingSkull - Red flaming skull",
-	["Q_ShadowFlamingSkull"] = "Q_ShadowFlamingSkull - Shadow purple flaming skull",
-	["Q_GreenGPS"]           = "Q_GreenGPS - Green map pin/GPS symbol",
-	["Q_RedGPS"]             = "Q_RedGPS - Red map pin/GPS symbol",
-	["Q_WhiteGPS"]           = "Q_WhiteGPS - White map pin/GPS symbol",
-	["Q_GreenTarget"]        = "Q_GreenTarget - Green target arrow",
-	["Q_RedTarget"]          = "Q_RedTarget - Red target arrow",
-	["Q_WhiteTarget"]        = "Q_WhiteTarget - White target arrow",
-	["Hunters_Mark"]         = "Hunters_Mark - Red Hunter's Mark Arrow",
+local TEXTURE_NAMES = {
+	"Reticule",
+	"RedArrow",
+	"NeonReticule",
+	"NeonRedArrow",
+	"RedChevronArrow",
+	"PaleRedChevronArrow",
+	"arrow_tip_green",
+	"arrow_tip_red",
+	"skull",
+	"circles_target",
+	"red_star",
+	"greenarrowtarget",
+	"BlueArrow",
+	"bluearrow1",
+	"gearsofwar",
+	"malthael",
+	"NewRedArrow",
+	"NewSkull",
+	"PurpleArrow",
+	"Shield",
+	"NeonGreenArrow",
+	"Q_FelFlamingSkull",
+	"Q_RedFlamingSkull",
+	"Q_ShadowFlamingSkull",
+	"Q_GreenGPS",
+	"Q_RedGPS",
+	"Q_WhiteGPS",
+	"Q_GreenTarget",
+	"Q_RedTarget",
+	"Q_WhiteTarget",
+	"Hunters_Mark",
 }
 
--- Add the directory prefix to the texture names
+-- Add the directory prefix to the texture names and localise the descriptions
+local TEXTURES = {}
 do
-	local oldTextures = TEXTURES
-	TEXTURES = {}
-
-	for texture, description in pairs(oldTextures) do
-		TEXTURES["Interface\\AddOns\\TargetNameplateIndicator\\Textures\\" .. texture] = description
+	for _, textureName in ipairs(TEXTURE_NAMES) do
+		local description = L[("Dropdown.Texture.%s.Desc"):format(textureName)]
+		TEXTURES["Interface\\AddOns\\TargetNameplateIndicator\\Textures\\" .. textureName] = ("%s - %s"):format(textureName, description)
 	end
 end
 
 -- The points that regions can be anchored to/by. Used for the texture and anchor point dropdowns.
-local REGION_POINTS = {
-	["TOP"]         = "Top - The centre-point of the top side of the rectange",
-	["RIGHT"]       = "Right - The centre-point of the right side of the rectange",
-	["BOTTOM"]      = "Bottom - The centre-point of the bottom side of the rectange",
-	["LEFT"]        = "Left - The centre-point of the left side of the rectange",
-	["TOPRIGHT"]    = "Top Right - The top right corner of the rectange",
-	["TOPLEFT"]     = "Top Left - The top left corner of the rectange",
-	["BOTTOMLEFT"]  = "Bottom Left - The bottom left corner of the rectange",
-	["BOTTOMRIGHT"] = "Bottom Right - The bottom right corner of the rectange",
-	["CENTER"]      = "Centre - The centre-point of the rectange",
+local REGION_POINT_NAMES = {
+	"TOP",
+	"RIGHT",
+	"BOTTOM",
+	"LEFT",
+	"TOPRIGHT",
+	"TOPLEFT",
+	"BOTTOMLEFT",
+	"BOTTOMRIGHT",
+	"CENTER",
 }
+
+-- Localise the region point descriptions
+local REGION_POINTS = {}
+do
+	for _, regionPoint in ipairs(REGION_POINT_NAMES) do
+		REGION_POINTS[regionPoint] = L[("Dropdown.RegionPoint.%s.Desc"):format(regionPoint)]
+	end
+end
 
 -- A pattern that matches a number with an optional decimal part
 local NUMBER_PATTERN = "^%d+%.?%d*$"
@@ -107,7 +116,15 @@ local function setNumber(info, val)
 	set(info, val)
 end
 
-local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
+local function getName(info)
+	return L[("Option.UnitReactionType.%s.Name"):format(info[#info])]
+end
+
+local function getDesc(info)
+	return L[("Option.UnitReactionType.%s.Desc"):format(info[#info])]
+end
+
+local function CreateUnitRectionTypeConfigTable(unit, unitReactionType, order)
 	local index = 0
 
 	local function nextIndex()
@@ -116,20 +133,20 @@ local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
 	end
 
 	return {
-		name = unitReactionType,
-		desc = desc,
+		name = L[("Group.%s.Name"):format(unitReactionType)],
+		desc = L[("Group.%s.%s.Desc"):format(unit, unitReactionType)],
 		order = order,
 		type = "group",
 		args = {
 			enable = {
-				name = "Enable",
-				desc = "Enables/disables the indicator for this unit reaction type",
+				name = getName,
+				desc = getDesc,
 				order = nextIndex(),
 				type = "toggle",
 			},
 			texture = {
-				name = "Texture",
-				desc = "The texture to use for the indicator",
+				name = getName,
+				desc = getDesc,
 				order = nextIndex(),
 				width = "full",
 				type = "select",
@@ -149,8 +166,8 @@ local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
 				imageHeight = 100,
 			},
 			width = {
-				name = "Texture Width",
-				desc = "The width of the texture",
+				name = getName,
+				desc = getDesc,
 				order = nextIndex(),
 				type = "input",
 				pattern = NUMBER_PATTERN,
@@ -158,8 +175,8 @@ local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
 				set = setNumber,
 			},
 			height = {
-				name = "Texture Height",
-				desc = "The height of the texture",
+				name = getName,
+				desc = getDesc,
 				order = nextIndex(),
 				type = "input",
 				pattern = NUMBER_PATTERN,
@@ -167,8 +184,8 @@ local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
 				set = setNumber,
 			},
 			opacity = {
-				name = "Texture Opacity",
-				desc = "The opacity of the texture. 1 is fully opaque, 0 is transparent.",
+				name = getName,
+				desc = getDesc,
 				order = nextIndex(),
 				type = "input",
 				pattern = NUMBER_PATTERN,
@@ -176,8 +193,8 @@ local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
 				set = setNumber,
 			},
 			texturePoint = {
-				name = "Texture Point",
-				desc = "The point of the texture that should be anchored to the nameplate",
+				name = getName,
+				desc = getDesc,
 				order = nextIndex(),
 				width = "full",
 				type = "select",
@@ -185,8 +202,8 @@ local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
 				style = "dropdown",
 			},
 			anchorPoint = {
-				name = "Anchor Point",
-				desc = "The point of the nameplate the texture should be anchored to",
+				name = getName,
+				desc = getDesc,
 				order = nextIndex(),
 				type = "select",
 				width = "full",
@@ -194,8 +211,8 @@ local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
 				style = "dropdown",
 			},
 			xOffset = {
-				name = "X Offset",
-				desc = "The x offset of the texture relative to the anchor point. Negative values move the texture left, positive values move the texture right.",
+				name = getName,
+				desc = getDesc,
 				order = nextIndex(),
 				type = "input",
 				pattern = NUMBER_PATTERN,
@@ -203,8 +220,8 @@ local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
 				set = setNumber,
 			},
 			yOffset = {
-				name = "Y Offset",
-				desc = "The y offset of the texture relative to the anchor point. Negative values move the texture down, positive values move the texture up.",
+				name = getName,
+				desc = getDesc,
 				order = nextIndex(),
 				type = "input",
 				pattern = NUMBER_PATTERN,
@@ -215,20 +232,20 @@ local function CreateUnitRectionTypeConfigTable(unitReactionType, order, desc)
 	}
 end
 
-local function CreateUnitConfigTable(unit, selfDesc, friendlyDesc, hostileDesc)
+local function CreateUnitConfigTable(unit)
 	return {
-		name = unit,
+		name = L[("Group.%s.Name"):format(unit)],
 		type = "group",
 		childGroups = "tab",
 		args = {
 			enable = {
-				name = "Enable",
-				desc = "Enables/disables the indicator for this unit",
+				name = L["Option.Unit.enable.Name"],
+				desc = L["Option.Unit.enable.Desc"],
 				type = "toggle",
 			},
-			self = CreateUnitRectionTypeConfigTable("self", 1, selfDesc),
-			friendly = CreateUnitRectionTypeConfigTable("friendly", 2, friendlyDesc),
-			hostile = CreateUnitRectionTypeConfigTable("hostile", 3, hostileDesc),
+			self = CreateUnitRectionTypeConfigTable(unit, "self", 1),
+			friendly = CreateUnitRectionTypeConfigTable(unit, "friendly", 2),
+			hostile = CreateUnitRectionTypeConfigTable(unit, "hostile", 3),
 		},
 	}
 end
@@ -238,28 +255,13 @@ local options = {
 	type = "group",
 	args = {
 		indicators = {
-			name = "Unit Indicator Options",
+			name = L["Group.indicators.Name"],
 			order = 1,
 			type = "group",
 			args = {
-				target = CreateUnitConfigTable(
-					"target",
-					"These options are used when targeting yourself",
-					"These options are used for friendly targets",
-					"These options are used for hostile targets"
-				),
-				mouseover = CreateUnitConfigTable(
-					"mouseover",
-					"These options are used when mousing over yourself",
-					"These options are used for friendly mouseovers",
-					"These options are used for hostile mouseovers"
-				),
-				focus = CreateUnitConfigTable(
-					"focus",
-					"These options are used when focusing yourself",
-					"These options are used for friendly focuses",
-					"These options are used for hostile focuses"
-				),
+				target = CreateUnitConfigTable("target"),
+				mouseover = CreateUnitConfigTable("mouseover"),
+				focus = CreateUnitConfigTable("focus"),
 			},
 		},
 	},
