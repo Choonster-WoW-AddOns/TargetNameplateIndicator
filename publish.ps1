cb3918c6da693c67ca857ed14cf8170cc94429e4
Choonster TheMage <Choonster.2010@gmail.com>
2022-04-25 01:58:40 +1000

    Copy enUS locale file manually in publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -11,3 +11,17 @@ $global:WOW_PACKAGER = wsl wslpath $(Resolve-Path '~\source\repos\packager\relea
 Import-Module '~\source\repos\PublishAddon\wow.psm1'
 
 Publish-Addon
+
+# Copy the enUS locale file with translations manually
+('_classic_', '_classic_era_', '_retail_') | 
+ForEach-Object {
+    $gameDir = $_
+    $enUSPath = Join-Path 'Locales' 'enUS.lua'
+
+    $sourcePath = Join-Path '.' $enUSPath
+    $destPath = Join-Path $WOW_HOME $gameDir 'Interface' 'AddOns' 'TargetNameplateIndicator' $enUSPath
+
+    if (Test-Path $destPath) {
+        Copy-Item $sourcePath $destPath -Force
+    }
+}
