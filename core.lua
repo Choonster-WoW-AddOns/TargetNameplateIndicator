8d4cf50abe2a2d43ed3dedcf5126d003e750b171
Choonster TheMage <Choonster.2010@gmail.com>
2026-09-13 18:53:34 +1000

    Add missing defaults for friendly soft targets - Exclude defaults for non-applicable reaction types for each soft target unit - Fix indicator priority checks exiting on first matching unit   - This was causing the target and hostile soft target indicators to display at the same time

--- core.lua
+++ core.lua
@@ -67,19 +67,25 @@ do
 		}
 	end
 
-	local function CreateUnitDefaults()
+	---
+	---@param includeSelf? boolean
+	---@param includeFriendly? boolean
+	---@param includeHostile? boolean
+	---@return table
+	local function CreateUnitDefaults(includeSelf, includeFriendly, includeHostile)
 		return {
 			enable = true,
-			self = CreateUnitReactionTypeDefaults(),
-			friendly = CreateUnitReactionTypeDefaults(),
-			hostile = CreateUnitReactionTypeDefaults(),
+			self = includeSelf ~= false and CreateUnitReactionTypeDefaults() or nil,
+			friendly = includeFriendly ~= false and CreateUnitReactionTypeDefaults() or nil,
+			hostile = includeHostile ~= false and CreateUnitReactionTypeDefaults() or nil,
 		}
 	end
 
 	defaults = {
 		profile = {
 			target = CreateUnitDefaults(),
-			softenemy = CreateUnitDefaults(),
+			softenemy = CreateUnitDefaults(false, false, true),
+			softfriend = CreateUnitDefaults(true, true, false),
 			mouseover = CreateUnitDefaults(),
 			focus = CreateUnitDefaults(),
 			targettarget = CreateUnitDefaults(),
@@ -195,19 +201,19 @@ end
 -- - If a lower priority indicator is displaying, it is hidden and this returns true.
 -- - If an equal or higher priority indicator is displaying, this returns false.
 function Indicator:CheckAndHideLowerPriorityIndicators()
+	local shouldDisplay = true
+
 	for unit, indicator in pairs(TNI.Indicators) do
 		if indicator.enabled and self.unit ~= indicator.unit and UnitIsUnit(self.unit, unit) then -- If the indicator is for a different unit token but it's the same unit,
-			if self.priority > indicator.priority then                                      -- If this indicator is a higher priority, hide the other indicator and return true
+			if self.priority > indicator.priority then                                      -- If this indicator is a higher priority, hide the other indicator
 				indicator:Update()
-				return true
-			else -- If this indicator is a lower or equal priority, return false
-				return false
+			else                                                                            -- If this indicator is a lower or equal priority, hide it
+				shouldDisplay = false
 			end
 		end
 	end
 
-	-- No other indicator is displaying, return true
-	return true
+	return shouldDisplay
 end
 
 local GetNamePlateUnit
@@ -411,10 +417,11 @@ local MouseoverIndicator = CreateNonTargetIndicator("mouseover", 10)
 ------
 
 ---@diagnostic disable-next-line: unused-local
-local SoftEnemyIndicator = CreateNonTargetIndicator("softenemy", 80)
+local SoftEnemyIndicator = CreateNonTargetIndicator("softenemy", 70)
 
 ---@diagnostic disable-next-line: unused-local
 local SoftFriendIndicator = CreateNonTargetIndicator("softfriend", 80)
+
 
 ------
 -- Focus Indicator
